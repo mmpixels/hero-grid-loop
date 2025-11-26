5e5bdb633a02fb1ce464f758225c8f3065980663
mmpixels <manasvimodi.mm@gmail.com>
2025-11-26 19:50:47 +0530

    fix: tailwind content paths for client folder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,15 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: [
+  "./client/index.html",
+  "./client/src/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{ts,tsx}",
+  "./components/**/*.{ts,tsx}",
+  "./app/**/*.{ts,tsx}",
+  "./src/**/*.{ts,tsx}"
+],
+
   prefix: "",
   theme: {
     container: {
